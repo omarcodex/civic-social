84c75c80360b93ae1418dc70a2417232f5c18d5d
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-13 17:31:25 -0800

    Set up main page to make API url call successfully

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+  require 'net/http'
+  require 'uri'
+
+  def index
+
+    # Setting up the cURL-like URL request (see https://propublica.github.io/congress-api-docs/#responses).
+
+    uri = URI.parse("https://api.propublica.org/congress/v1/114/house/members.json")
+
+    request = Net::HTTP::Get.new(uri)
+    request["X-Api-Key"] = ENV['API_KEY']
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    # Show the response in an appropriate form.
+    render :json => response.body
+  end
+
+end
