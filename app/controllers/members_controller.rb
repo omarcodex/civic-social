9df396b51cc444bb6aed3c4642e0cb7dacbb1cb7
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-08 21:15:28 -0800

    Add plain list members names

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,88 @@
+class MembersController < ApplicationController
+
+  require 'net/http'
+  require 'json'
+
+  def index
+
+    raw_senate_members_data = JSON.parse(get_members_data_from_API(115,"senate").body)
+
+    raw_house_members_data = JSON.parse(get_members_data_from_API(115,"house").body)
+
+    senate_members = raw_senate_members_data["results"].first["members"]
+    house_members = raw_house_members_data["results"].first["members"]
+
+    @all_members = []
+    @member = {}
+
+    senate_members.each do |hash|
+      member = {}
+      member[:first_name] = hash["first_name"]
+      member[:last_name] = hash["last_name"]
+      member[:link] = hash["api_uri"]
+      @all_members << member
+     end
+
+    house_members.each do |hash|
+       member = {}
+       member[:first_name] = hash["first_name"]
+       member[:last_name] = hash["last_name"]
+       member[:link] = hash["api_uri"]
+       @all_members << member
+    end
+
+  render :index
+
+  end
+
+  def show
+
+    # @bill_id = params[:id]
+    # uri = URI.parse("https://api.propublica.org/congress/v1/115/bills/#{@bill_id}.json")
+    #
+    # request = Net::HTTP::Get.new(uri)
+    # request["X-Api-Key"] = ENV['API_KEY']
+    # req_options = {
+    #   use_ssl: uri.scheme == "https",
+    # }
+    #
+    # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    #   http.request(request)
+    # end
+    #
+    # raw_data_all = JSON.parse(response.body)
+    #
+    # @raw_data_result_only = raw_data_all["results"].first
+    #
+    # render :show
+
+  end
+
+
+  def patch
+  end
+
+  def delete
+  end
+
+  private
+
+  def get_members_data_from_API(congress, chamber)
+
+    uri = URI.parse("https://api.propublica.org/congress/v1/#{congress}/#{chamber}/members.json")
+
+    request = Net::HTTP::Get.new(uri)
+    request["X-Api-Key"] = ENV['API_KEY']
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    return response
+
+  end
+
+end
